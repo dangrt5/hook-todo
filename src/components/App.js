6b1ee2d4660f96c91b4af13cdb46408ef883dcf6
dangrt5 <randy.dang05@gmail.com>
2019-02-19 16:23:48 -0800

    initial hook commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Hook from "./Hook";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        React Hooks Intro
+        <Hook />
+      </div>
+    );
+  }
+}
+
+export default App;
